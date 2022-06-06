6ddb0b76d119c89a22de45fc73995c99ddfb372c
Thomas Pendleton <thomas6576@yahoo.com>
2022-06-05 22:37:14 -0700

    setup of new project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <h2>menu project setup</h2>;
+  return <Menu />
 }
 
-export default App;
+export default App
